3d5eb7bf4f87e11bda8d193b98ce7fb97c0c3c04
zhuchentong <2037630@163.com>
2019-04-16 18:35:41 +0800

    feat(package.json): update package.json

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['cz'],
+  rules: {
+    'type-empty': [0, 'never'],
+    'scope-empty': [0, 'never'],
+    'scope-case': [2, 'always', 'lowerCase'],
+    'header-max-length': [2, 'always', 80]
+  }
+}
